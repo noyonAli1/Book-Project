e183a2977dd593c8b8c1d37aa31546cbaa2ac475
noyonAli1 <noyon.ali713703@gmail.com>
2026-05-02 21:16:27 +0600

    fix database

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,20 +1,42 @@
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+// import dns from "dns";
+// dns.setDefaultResultOrder("ipv4first");
+
+// const client = new MongoClient(process.env.AUTH_DB_URI);
+// const db = client.db("bookdbsur");
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(db, {
+    
+//     client
+//   }),
+//   emailAndPassword:{
+//     enabled:true
+//   }
+// });
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
 import dns from "dns";
+
 dns.setDefaultResultOrder("ipv4first");
 
 const client = new MongoClient(process.env.AUTH_DB_URI);
+
+
+await client.connect();
+
 const db = client.db("bookdbsur");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    
-    client
+    client,
   }),
-  emailAndPassword:{
-    enabled:true
-  }
+  emailAndPassword: {
+    enabled: true,
+  },
 });
-
